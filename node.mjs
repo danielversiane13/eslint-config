b79c9cda694a56ace07d083735769530f89eddef
Daniel Versiane <daniel.versiane@gmail.com>
2026-08-24 19:35:00 -0300

    feat: add node config

---
+++ node.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals'
+
+import config from './base.mjs'
+
+export default [
+  ...config,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+]
